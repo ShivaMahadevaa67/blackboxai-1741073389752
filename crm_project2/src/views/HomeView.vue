18f7054f5b899e62d8afa5f04be34776e8a654c6
ShivaMahadevaa67 <shivamahadev477@gmail.com>
2025-03-04 13:15:11 +0545

    Add crm_project2/src/views/HomeView.vue 1741073409946

---
+++ crm_project2/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import DashboardLayout from '@/layouts/DashboardLayout.vue';
+
+</script>
+
+<template>
+  <DashboardLayout>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+  </DashboardLayout>
+</template>
